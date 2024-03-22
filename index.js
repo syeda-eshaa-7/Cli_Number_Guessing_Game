ef48d5b242058822c255ce82bbfc6f37e4e1d47d
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-03-22 15:16:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/user/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess the number between 1/6!",
+    }
+]);
+if (answer.userGuessedNumber == randomNumber) {
+    console.log("congretulations!you guessed tha right number");
+}
+else {
+    console.log(" you guess tha wrong number");
+}
+;
